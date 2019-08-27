a959d3cd7bf5b7c0d3ca5ac4d72ce15b85881cf7
lbarosi <lbarosi@gmail.com>
2019-08-27 07:35:39 -0300

    Presentation included

--- scrap/FFT.py
+++ scrap/FFT.py
@@ -1,10 +1,14 @@
+https://ourcodingclub.github.io/2019/01/07/pandas-time-series.html
+https://bicorner.com/2015/11/16/time-series-analysis-using-ipython/
+http://exnumerus.blogspot.com/2011/12/how-to-remove-noise-from-signal-using.html
+
+https://timsainburg.com/noise-reduction-python.html
 # https://www.oreilly.com/library/view/elegant-scipy/9781491922927/ch04.html
-# https://martinos.org/mne/stable/auto_examples/time_frequency/plot_compute_raw_data_spectrum.html
+
 # http://gael-varoquaux.info/scipy-lecture-notes/intro/scipy.html#fast-fourier-transforms-scipy-fftpack
-# https://www.elenacuoco.com/2016/09/22/power-spectral-density/
-# https://joseph-long.com/writing/from-sky-coordinates-to-pixels-and-back/
+
+
 #
-# https://www.datascience.com/learn-data-science/fundamentals/introduction-to-anomaly-detection-python-data-science
 #
 # http://python4mpia.github.io/core/core.html
 #
@@ -23,3 +27,52 @@ mastercat_file = (
     "http://monoceros.astro.yale.edu/RELEASE_V4.1.5/3dhst.v4.1.5.master.fits.gz"
 )
 pmcat = Table.read(mastercat_file).to_pandas()
+
+print(type(pmcat))
+pandas.set_option('display.max_columns', 500)  # print all columns in .head()
+pmcat.shape
+
+
+# Demo some basic filtering.  Not used below.
+zlo = 2.0
+zhi = 3.0
+Mlo = 9.0
+Mhi = 9.5
+print(pmcat[pmcat['use_phot'].eq(1)].shape)   # filter on good photometry
+print(pmcat[pmcat['z_best'].between(zlo,zhi)].shape)  # Filter redshift
+print(pmcat[pmcat['lmass'].between(Mlo,Mhi)].shape)   # Filter stellar mass
+filt = pmcat['z_best'].between(zlo,zhi) & pmcat['lmass'].between(Mlo,Mhi) & pmcat['use_phot'].eq(1)
+print(pmcat[filt].shape) #all 3 filters
+
+# Read in Arjen's catalogs of morphological parameters, for each field
+#download_file("http://www.mpia-hd.mpg.de/homes/vdwel/allfields.tar")
+# below is really kludgy.  Did by hand
+# mkdir VanderWel; cd VanderWel; mv ../allfields.tar . ; tar -xvf allfields.tar
+# I then edited by hand the headers, stripping out the initial "#  "
+vdw_H_files = ("VanderWel/aegis/aegis_3dhst.v4.1_f125w.galfit", "VanderWel/goodss/goodss_3dhst.v4.1_f125w.galfit", "VanderWel/cosmos/cosmos_3dhst.v4.1_f125w.galfit", "VanderWel/uds/uds_3dhst.v4.1_f125w.galfit", "VanderWel/goodsn/goodsn_3dhst.v4.1_f125w.galfit")
+vdw_fields = ('aegis', 'goodss', 'cosmos', 'uds', 'goodsn')
+vdw_df = {}
+for ii, label in enumerate(vdw_fields):
+    #jrr.util.strip_pound_before_colnames(vdw_H_files[ii])  # strip # from header
+    vdw_df[label] = pandas.read_table(vdw_H_files[ii], delim_whitespace=True, comment="#", header=0)
+    vdw_df[label]['field'] = vdw_fields[ii]
+    vdw_df[label]['JRRID'] = vdw_fields[ii] + '.' + vdw_df[label]['NUMBER'].astype(str)
+vdw_all = pandas.concat(vdw_df)
+
+
+pmcat_jrr   = pmcat.set_index('JRRID', drop=False)
+vdw_all_jrr= vdw_all.set_index('JRRID', drop=False)
+pmcat_jrr.to_csv("pmcat.csv")
+vdw_all_jrr.to_csv("vdw_alljrr.csv")
+
+
+import pandas as pd
+import io
+import requests
+
+url = "http://www.sidc.be/silso/DATA/SN_m_tot_V2.0.txt"
+s = requests.get(url).content
+ds = pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+ds.describe()
+ds.head(5)
---
+++ scrap/TimeSeries.py
@@ -0,0 +1,126 @@
+#https://ourcodingclub.github.io/2019/01/07/pandas-time-series.html
+#https://towardsdatascience.com/from-scratch-bayesian-inference-markov-chain-monte-carlo-and-metropolis-hastings-in-python-ef21a29e25a
+#https://github.com/royalosyin/Python-Practical-Application-on-Climate-Variability-Studies
+#http://www.das.inpe.br/school/2009/lectures_feigelson/INPE_Feigelson_R.pdf
+#https://wwwmpa.mpa-garching.mpg.de/~ensslin/nifty-gallery/index.html
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from scipy import stats
+from statsmodels.graphics.api import qqplot
+import io
+import requests
+%matplotlib inline
+##############
+# Monthly data
+url = "http://www.sidc.be/silso/DATA/SN_m_tot_V2.0.txt"
+s = requests.get(url).content
+colnames = ['YEAR', 'MONTH', 'YM', 'ACTIVITY', 'C1', 'C2', 'C3']
+df = pd.read_table(io.StringIO(s.decode('utf-8')), names = colnames, header = None, delim_whitespace=True )
+df.head(5)
+
+df['DAY']=1
+df = df[['YEAR', 'MONTH', 'DAY', 'ACTIVITY']]
+df.index = pd.to_datetime(df[['YEAR','MONTH','DAY']])
+df = df[['ACTIVITY']]
+df.head(5)
+
+# You can select by datetime with partial match
+df.loc['1830']
+#############
+import seaborn as sns
+# Use seaborn style defaults and set the default figure size
+sns.set(rc={'figure.figsize':(11, 4)})
+df['ACTIVITY'].plot(linewidth=0.5);
+
+df = df.asfreq('M', method='ffill')
+df_year = df.resample('Y').mean()
+df_year.head(3)
+
+
+# Compute the centered 7-day rolling mean
+df_5y = df_year['ACTIVITY'].rolling(5, center=True).mean()
+df_5y.head(10)
+
+
+# Start and end of the date range to extract
+#start, end = '1780-01', '2017-06'
+# Plot daily, weekly resampled, and 7-day rolling mean time series together
+fig, ax = plt.subplots()
+ax.plot(df,
+linestyle='-', linewidth=0.5, color = "red", alpha = 0.5, label='Monthly')
+ax.plot(df_year,
+marker='.', linestyle='-', linewidth=0.5, label='Yearly')
+ax.plot(df_5y,
+marker='o', markersize=4, linestyle='-', label='Moving Window')
+ax.set_ylabel('Solar Activity')
+ax.legend();
+
+
+df.loc['1900':'1990'].resample("5y").median().diff().plot()
+
+pd.plotting.autocorrelation_plot(df.resample("1y").median())
+
+
+file = "../DATA/co2_mm_mlo.txt"
+colnames = ["year", "month", "decimal_date", "average", "interpolated", "trend", "days"]
+df = pd.read_table( file,
+                    names = colnames,
+                    header = None,
+                    delim_whitespace=True,
+                    comment = '#',
+                    skiprows = 72,
+                    na_values = [-99.99, -1] )
+df.head(3)
+
+df['Date'] = df['year']*100 + df['month']
+df['Date'] = pd.to_datetime(df['Date'], format='%Y%m', errors='ignore')
+df.set_index('Date', inplace=True)
+df.drop(["year", "month", "decimal_date", "interpolated",  "trend", "days"], axis=1, inplace=True)
+df.head(3)
+
+df.isnull().sum()
+# Filling missing values
+df = df.fillna(df.bfill())
+df.plot(title='Monthly CO2 (ppm)')
+
+# Decompose do not handle missing values
+decomposition = sm.tsa.seasonal_decompose(df, model='additive')
+fig = decomposition.plot()
+
+
+df_seasonal = decomposition.seasonal.asfreq('M', method='ffill')
+df_seasonal.head(3)
+
+
+pd.plotting.autocorrelation_plot(df_seasonal)
+plt.xlim([0,24])
+
+
+from scipy import signal
+
+n        = 100
+alpha    = 1
+noverlap = 50
+nfft     = 1024 #default value
+fs       = 1   #default value
+win      = signal.tukey(n, alpha)
+data     = df_seasonal['average'].values # convert vector
+
+f1, pxx1  = signal.welch(data, nfft=nfft, fs=fs, window=win, noverlap=noverlap)
+
+# process frequencies and psd
+pxx1 = pxx1/np.max(pxx1) # normalize the psd values
+plt.plot(1.0/f1, pxx1, label='welch')
+plt.title('CO2 Spectrum via welch');
+plt.xlabel('Months')
+plt.xlim([0,24])
+
+
+#https://hadrienj.github.io/posts/Preprocessing-for-deep-learning/
+
+https://hub.gke.mybinder.org/user/elegant-scipy-notebooks-yqah45st/notebooks/notebooks/ch3.ipynb
+https://hub.gke.mybinder.org/user/elegant-scipy-notebooks-yqah45st/notebooks/notebooks/ch4.ipynb
+http://gael-varoquaux.info/scipy-lecture-notes/intro/scipy.html#fast-fourier-transforms-scipy-fftpack
